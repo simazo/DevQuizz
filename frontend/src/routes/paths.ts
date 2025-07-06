08efc73804c87761a1ec791e004678496169a94d
f.t <pero@peronoMacBook-Air.local>
2025-07-06 20:32:06 +0900

    フロント側のルーティングミス修正

--- frontend/src/routes/paths.ts
+++ frontend/src/routes/paths.ts
@@ -2,7 +2,7 @@ export const PATHS = {
   HOME: '/',
   CATEGORY: '/category/:categoryKey',
   RESULT: '/result',
-  QUESTION_DETAIL: '/question/:id',
+  QUESTION_DETAIL: '/questions/:id',
 };
 
 export const generateCategoryPath = (categoryKey: string) =>
